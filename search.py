194ce220706f84cc34b818ae14feedbff32530c3
ilias <illibidane27@gmail.com>
2021-10-26 12:27:59 +0200

    test5

--- search.py
+++ search.py
@@ -9,7 +9,7 @@
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
 # Student side autograding was added by Brad Miller, Nick Hay, and
-# Pieter Abbeel (pabbeel@cs.berkeley.edu). TEST3
+# Pieter Abbeel (pabbeel@cs.berkeley.edu). TEST5
 
 
 """
