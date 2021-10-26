72fd3d7881b2f7e4f2433fbb6e1227e2cd9c3169
iliB901 <92427943+iliB901@users.noreply.github.com>
2021-10-26 12:16:46 +0200

    Update search.py

--- search.py
+++ search.py
@@ -9,7 +9,7 @@
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
 # Student side autograding was added by Brad Miller, Nick Hay, and
-# Pieter Abbeel (pabbeel@cs.berkeley.edu). TESTETST
+# Pieter Abbeel (pabbeel@cs.berkeley.edu). TEST3
 
 
 """
